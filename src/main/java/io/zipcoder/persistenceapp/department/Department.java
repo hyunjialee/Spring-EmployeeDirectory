891f089354124e18213374ba9ff830badce8016e
hyunjialee <hyjialee@gmail.com>
2023-12-29 14:08:20 -0500

    starting

---
+++ src/main/java/io/zipcoder/persistenceapp/department/Department.java
@@ -0,0 +1,7 @@
+package io.zipcoder.persistenceapp.department;
+
+public class Department {
+    private int departmentNumber;
+    private String departmentName;
+    private String manager;
+}
---
+++ src/main/java/io/zipcoder/persistenceapp/employee/Employee.java
@@ -0,0 +1,15 @@
+package io.zipcoder.persistenceapp.employee;
+
+public class Employee {
+
+    private int employeeNumber;
+    private String firstName;
+    private String lastName;
+    private String title;
+    private int phoneNumber;
+    private String email;
+    private String date;
+    private String manager;
+    private int departmentNumber;
+
+}
