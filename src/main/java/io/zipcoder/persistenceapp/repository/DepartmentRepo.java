dccf4f9d261253f824663a164c2ad9617095529c
hyunjialee <hyjialee@gmail.com>
2024-01-01 19:49:52 -0500

    added department controller, repo, service

---
+++ src/main/java/io/zipcoder/persistenceapp/repository/DepartmentRepo.java
@@ -0,0 +1,9 @@
+package io.zipcoder.persistenceapp.repository;
+
+import io.zipcoder.persistenceapp.model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepo extends JpaRepository<Department, Integer> {
+}
---
+++ src/main/java/io/zipcoder/persistenceapp/service/DepartmentService.java
@@ -0,0 +1,8 @@
+package io.zipcoder.persistenceapp.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentService {
+}
